7b12029753ba78aa12ad3903b2f91e06d2b4d13a
KevinVan720 <kevin950720@gmail.com>
2022-03-12 10:31:06 -0500

    update animation time scale

--- lib/theme/themes/puzzle_theme_animations.dart
+++ lib/theme/themes/puzzle_theme_animations.dart
@@ -2,15 +2,15 @@
 abstract class PuzzleThemeAnimationDuration {
   /// The duration of a text style animation.
   /// The animation may happen when a theme is changed.
-  static const textStyle = Duration(milliseconds: 333);
+  static const textStyle = Duration(milliseconds: 200);
 
   /// The duration of a background color change animation.
   /// The animation may happen when a theme is changed.
-  static const backgroundColorChange = Duration(milliseconds: 333);
+  static const backgroundColorChange = Duration(milliseconds: 200);
 
   /// The duration of a logo change animation.
   /// The animation may happen when a theme is changed.
-  static const logoChange = Duration(milliseconds: 333);
+  static const logoChange = Duration(milliseconds: 200);
 
   /// The duration of a puzzle tile scale change animation.
   /// The animation may happen when a user hovers over a puzzle tile.
